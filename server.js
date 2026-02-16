7e8f85072972da64909e3e63673585ed47bd0669
naphattharawat <naphattharawat@gmail.com>
2026-02-16 23:17:47 +0700

    update

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(function (err, req, res, next) {
   res.send({ ok: false, error: err.message })
 });
 
-let port = 80;
+let port = 4000;
 
 app.listen(port, function () {
   console.log(`Web listening on port ${port}!`)
