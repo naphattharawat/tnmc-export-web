180fc77e0213fef39bfcd54cd0e496b8ab506ed9
naphattharawat <naphattharawat@gmail.com>
2026-02-24 08:56:20 +0700

    แก้ไข callbacl

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -30,7 +30,8 @@ export class LoginComponent implements OnInit {
     }
     this.isLoading = true;
     const clientId = 'MU9VN1luaUFTT1BaQmNGQlVxaTBQckNyVXVkdnVQc3g';
-    const redirectUri = 'http://localhost:4200/callback/thaid';
+    const redirectUri = 'http://172.16.30.149/callback/thaid';
+    // const redirectUri = 'http://localhost:4200/callback/thaid';
     const state = Math.random().toString(36).substring(2);
     const url = `https://imauth.bora.dopa.go.th/api/v2/oauth2/auth/?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=pid&state=${state}`;
     window.location.href = url;
